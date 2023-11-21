a0cd558df297657faa14abd7788a0292852969ad
inezkaj <ienzkajed@icloud.com>
2023-11-21 17:27:38 +0100

    feat: change src folder

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,19 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('ul.gallery');
+
+const images = galleryItems.map(item => {
+  return `<li>
+             <a class="gallery__item" href="${item.original}">
+                <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+             </a>
+          </li>`;
+});
+
+gallery.insertAdjacentHTML(`beforeend`, images.join(''));
+
+const lightbox = new SimpleLightbox('ul.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const localStorageKey = 'feedback-form-state';
+
+if (localStorage.getItem(localStorageKey)) {
+  let storageValues = JSON.parse(localStorage.getItem(localStorageKey));
+  form.elements.message.value = storageValues.message ?? '';
+  form.elements.email.value = storageValues.email ?? '';
+}
+
+form.addEventListener(
+  'input',
+  throttle(() => {
+    let values = {
+      email: form.elements.email.value,
+      message: form.elements.message.value,
+    };
+    localStorage.setItem(localStorageKey, JSON.stringify(values));
+  }, 500)
+);
+
+form.addEventListener('submit', ev => {
+  ev.preventDefault();
+  localStorage.removeItem(localStorage);
+  form.reset();
+});
